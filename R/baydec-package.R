a2ca26a285742ac7bfc1d325268b0fb6b5f433aa
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2024-06-25 21:28:09 +0100

    namespace update

--- R/baydec-package.R
+++ R/baydec-package.R
@@ -1,6 +1,15 @@
+#' The 'baydec' package
+#'
+#' @description Bayesian deconvolution
+#' @aliases baydec
+#' @useDynLib baydec, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.2.9000. https://mc-stan.org
+#'
+#'
 #' @keywords internal
 "_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
 NULL
