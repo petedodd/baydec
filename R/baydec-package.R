e12130505ab6ff96208b64bacebb35f7271baff2
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2024-06-26 12:42:04 +0100

    correct config for build

--- R/baydec-package.R
+++ R/baydec-package.R
@@ -1,15 +1,12 @@
-#' The 'baydec' package
-#'
-#' @description Bayesian deconvolution
-#' @aliases baydec
-#' @useDynLib baydec, .registration = TRUE
-#' @import methods
-#' @import Rcpp
-#' @importFrom rstan sampling
-#' @references
-#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.2.9000. https://mc-stan.org
-#'
-#'
 #' @keywords internal
+#' @import Rcpp
+#' @import methods
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+#' @useDynLib baydec, .registration = TRUE
+#' 
 "_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
